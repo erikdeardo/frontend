7de29e7cc9bac4ebc3549dd918ce410dac4282f7
erikdeardo <64653122+erikdeardo@users.noreply.github.com>
2020-06-23 20:07:40 +0700

    Add files via upload

---
+++ ClubFinder/app.js
@@ -0,0 +1,5 @@
+import "./src/script/component/app-bar.js";
+import main from "./src/script/view/main.js";
+import getCovid from "./src/script/view/main2.js";
+document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", getCovid);
